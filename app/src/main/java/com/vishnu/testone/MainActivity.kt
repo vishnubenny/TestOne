66e387728cd683444f96c00f7a5561b39952b6f6
vishnubenny <vishnubenny@gmail.com>
2021-11-05 20:19:17 +0530

    task(Android: Fragment navigation): Add initial fragment navigation set-up Ticket-XXX (#6)
    Co-authored-by: Vishnu Benny <vishnu.benny@powerschool.com>

--- app/src/main/java/com/vishnu/testone/MainActivity.kt
+++ app/src/main/java/com/vishnu/testone/MainActivity.kt
@@ -1,19 +1,9 @@
 package com.vishnu.testone
 
-import android.os.Bundle
-import androidx.fragment.app.commit
 import com.vishnu.testone.base.BaseActivity
 import com.vishnu.testone.databinding.ActivityMainBinding
-import com.vishnu.testone.webbrowser.BrowserFragment
 
 class MainActivity : BaseActivity<ActivityMainBinding>() {
 
     override fun getViewBinding() = ActivityMainBinding.inflate(layoutInflater)
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        supportFragmentManager.commit {
-            replace(binding.vwFragmentContainer.id, BrowserFragment())
-        }
-    }
 }
